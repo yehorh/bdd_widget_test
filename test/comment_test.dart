99e39cbdb3b2a0dff07125bff2dbc541542d672b
Oleksandr Leushchenko <olexa.le@gmail.com>
2025-10-06 17:44:05 +0300

    Add Gherkin comments support

--- test/comment_test.dart
+++ test/comment_test.dart
@@ -39,6 +39,43 @@ void main() {
     expect(feature.dartContent, expectedFeatureDart);
   });
 
+  test('Gherkin comments are ignored', () {
+    const featureFile = '''
+# This is a comment
+
+Feature: Testing feature
+  #One more comment
+  Scenario: Testing scenario
+# Another comment
+    Given the app is running
+''';
+
+    const expectedFeatureDart = '''
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import './step/the_app_is_running.dart';
+
+void main() {
+  group(\'\'\'Testing feature\'\'\', () {
+    testWidgets(\'\'\'Testing scenario\'\'\', (tester) async {
+      await theAppIsRunning(tester);
+    });
+  });
+}
+''';
+
+    final feature = FeatureFile(
+      featureDir: 'test.feature',
+      package: 'test',
+      input: featureFile,
+    );
+    expect(feature.dartContent, expectedFeatureDart);
+  });
+
   test('Comments after first feature are ignored', () {
     const featureFile = '''
 Feature: Testing feature
